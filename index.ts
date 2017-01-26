8a91c112d8f5bcc4036b3bb26fecc1d5219d2d0e
nicoandresr <nicoandres.rodriguez@gmail.com>
2017-01-26 15:25:36 -0500

    Add email regex

--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
-export const name = '[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ/\s]+';
+export const name = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,400}$';
+export const email = '\\w+([-+.\']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*';

ef4913f8270e5dc344797b6d633cd2653940a292
nicoandresr <nicoandres.rodriguez@gimail.com>
2017-01-27 11:11:33 -0500

    Include guid regex

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export const name = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,400}$';
+export const lettersNumbers400 = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,400}$';
 export const email = '^\\w+([-+.\']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$';
+export const guid = '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$';

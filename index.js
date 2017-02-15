ab194ca90dea66e5fbaaba844de315aa2da6321b
nicoandresr <nicoandres.rodriguez@gimail.com>
2017-02-15 15:29:58 -0500

    Include new expresion regular for strings 150 lenght

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
 exports.lettersNumbers400 = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,400}$';
+exports.lettersNumbers150 = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,150}$';
 exports.email = '^\\w+([-+.\']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$';
 exports.guid = '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$';
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export const lettersNumbers400 = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,400}$';
+export const lettersNumbers150 = '^[a-zA-Z0-9ñÑáÁéÉíÍóÓúÚöÖüÜ\\s]{1,150}$';
 export const email = '^\\w+([-+.\']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$';
 export const guid = '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$';
